e0aef861256051e17cdc8797df4fc0e0d223c57a
dksgksrlf <dksgksrlf12@gmail.com>
2022-12-16 11:03:33 +0900

    aop signature사용해서 핵심코드 메소드 이름 출력

--- src/main/java/com/sample/myapp/HelloLog.java
+++ src/main/java/com/sample/myapp/HelloLog.java
@@ -1,7 +1,13 @@
 package com.sample.myapp;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+
 public class HelloLog {
-	public static void log() {
+	public static void log(JoinPoint joinPoint) {
+		Signature signature = joinPoint.getSignature();
+		String methodName = signature.getName();
+		System.out.println(methodName+" 메소드가 호출됩니다.");
 		System.out.println(">>>LOG<<< : "+new java.util.Date());
 	}
 }
